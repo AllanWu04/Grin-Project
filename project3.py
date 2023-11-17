80a4d73fcd97d602e2aeb7a8594bf7f586751fd2
Allan Wu <allankw@uci.edu>
2023-11-17 11:02:10 -0800

    Converted project3.py processing lines from for loop to while loop.
    After attempting to implement goto using a for loop, I found that using a while loop would be better since it would allow me to jump around to certain values in the list and allow me to go line by line.

--- project3.py
+++ project3.py
@@ -18,34 +18,43 @@ def main() -> None:
     convert_commands = grin.convert_to_grin_tokens(commands)
     all_var_values = dict()
     all_label_lines = grin.label_line(convert_commands)
+    line_tracker = 0
     if convert_commands is not None:
         try:
-            for line in convert_commands:
-                if line[0].kind() == grin.GrinTokenKind.LET:
-                    grin.let_conversion(line, all_var_values)
-                elif line[0].kind() == grin.GrinTokenKind.PRINT:
-                    grin.print_conversion(line, all_var_values)
-                elif line[0].kind() == grin.GrinTokenKind.INNUM or line[0].kind() == grin.GrinTokenKind.INSTR:
-                    grin.instr_and_innum_conversion(line, all_var_values)
-                elif line[0].kind() == grin.GrinTokenKind.ADD:
-                    add = grin.Addition(line, all_var_values)
+            while line_tracker < len(convert_commands):
+                if convert_commands[line_tracker][0].kind() == grin.GrinTokenKind.LET:
+                    grin.let_conversion(convert_commands[line_tracker], all_var_values)
+                    line_tracker += 1
+                elif convert_commands[line_tracker][0].kind() == grin.GrinTokenKind.PRINT:
+                    grin.print_conversion(convert_commands[line_tracker], all_var_values)
+                    line_tracker += 1
+                elif convert_commands[line_tracker][0].kind() == grin.GrinTokenKind.INNUM or convert_commands[line_tracker][0].kind() == grin.GrinTokenKind.INSTR:
+                    grin.instr_and_innum_conversion(convert_commands[line_tracker], all_var_values)
+                    line_tracker += 1
+                elif convert_commands[line_tracker][0].kind() == grin.GrinTokenKind.ADD:
+                    add = grin.Addition(convert_commands[line_tracker], all_var_values)
                     add.add_values()
-                elif line[0].kind() == grin.GrinTokenKind.SUB:
-                    sub = grin.Subtraction(line, all_var_values)
+                    line_tracker += 1
+                elif convert_commands[line_tracker][0].kind() == grin.GrinTokenKind.SUB:
+                    sub = grin.Subtraction(convert_commands[line_tracker], all_var_values)
                     sub.subtract_values()
-                elif line[0].kind() == grin.GrinTokenKind.MULT:
-                    mult = grin.Multiplication(line, all_var_values)
+                    line_tracker += 1
+                elif convert_commands[line_tracker][0].kind() == grin.GrinTokenKind.MULT:
+                    mult = grin.Multiplication(convert_commands[line_tracker], all_var_values)
                     mult.multiply_values()
-                elif line[0].kind() == grin.GrinTokenKind.DIV:
-                    div = grin.Division(line, all_var_values)
+                    line_tracker += 1
+                elif convert_commands[line_tracker][0].kind() == grin.GrinTokenKind.DIV:
+                    div = grin.Division(convert_commands[line_tracker], all_var_values)
                     div.divide_values()
-                elif line[0].kind() == grin.GrinTokenKind.IDENTIFIER and line[1].kind() == grin.GrinTokenKind.COLON:
-                    get_label_command = line[2:]
+                    line_tracker += 1
+                elif convert_commands[line_tracker][0].kind() == grin.GrinTokenKind.IDENTIFIER and convert_commands[line_tracker][1].kind() == grin.GrinTokenKind.COLON:
+                    get_label_command = convert_commands[line_tracker][2:]
                     if get_label_command[0].kind() == grin.GrinTokenKind.END or get_label_command[0].kind() == grin.GrinTokenKind.DOT:
                         break
                     else:
-                        grin.encountered_label_line(line, all_var_values)
-                elif line[0].kind() == grin.GrinTokenKind.END or line[0].kind() == grin.GrinTokenKind.DOT:
+                        grin.encountered_label_line(convert_commands[line_tracker], all_var_values)
+                    line_tracker += 1
+                elif convert_commands[line_tracker][0].kind() == grin.GrinTokenKind.END or convert_commands[line_tracker][0].kind() == grin.GrinTokenKind.DOT:
                     break
         except RuntimeError:
             print("Sorry, a runtime error has occurred. Please try again!")
