9379d8f335459614124b4203640eb8d233b5fdbd
Allan Wu <allankw@uci.edu>
2023-11-16 22:16:45 -0800

    Created encountered_label_line function and tested it.
    If the program were to naturally encounter that line, it would treat the label as a normal command and run the command in the label accordingly.

--- grin/label_goto_gosub.py
+++ grin/label_goto_gosub.py
@@ -1,4 +1,7 @@
 from grin.token import GrinTokenKind
+from grin.grinconversion import *
+from grin.addsub import *
+from grin.multdiv import *
 
 def label_line(lines):
     """Returns a dictionary of labels with corresponding line number its on"""
@@ -7,3 +10,24 @@ def label_line(lines):
         if line[0].kind() == GrinTokenKind.IDENTIFIER and line[1].kind() == GrinTokenKind.COLON:
             dict_of_labels.update({line[0].text(): line[0].location().line()})
     return dict_of_labels
+
+def encountered_label_line(line, dict_of_values):
+    """Interprets what occurs after the label"""
+    if line[0].kind() == GrinTokenKind.LET:
+        let_conversion(line, dict_of_values)
+    elif line[0].kind() == GrinTokenKind.PRINT:
+        print_conversion(line, dict_of_values)
+    elif line[0].kind() == GrinTokenKind.INNUM or line[0].kind() == GrinTokenKind.INSTR:
+        instr_and_innum_conversion(line, dict_of_values)
+    elif line[0].kind() == GrinTokenKind.ADD:
+        add = Addition(line, dict_of_values)
+        add.add_values()
+    elif line[0].kind() == GrinTokenKind.MULT:
+        mult = Multiplication(line, dict_of_values)
+        mult.multiply_values()
+    elif line[0].kind() == GrinTokenKind.DIV:
+        div = Division(line, dict_of_values)
+        div.divide_values()
+
+
+
--- tests/grin/test_label_goto_gosub.py
+++ tests/grin/test_label_goto_gosub.py
@@ -16,3 +16,23 @@ class TestLabelGoSubGoTo(unittest.TestCase):
         label_dict = label_line(convert_tokens)
         self.assertEqual(label_dict, {"TEST": 2, "TEST2": 4})
 
+    def test_encounter_label_line(self):
+        user_values = ["TEST: LET A 5", "TEST2: LET B 10", "TEST3: ADD A B"]
+        convert_tokens = convert_to_grin_tokens(user_values)
+        all_values = dict()
+        for line in convert_tokens:
+            if line[0].kind() == GrinTokenKind.IDENTIFIER and line[1].kind() == GrinTokenKind.COLON:
+                get_label_command = line[2:]
+                encountered_label_line(get_label_command, all_values)
+        self.assertEqual(all_values, {"A": 15, "B": 10})
+
+    def test2_encounter_label_line(self):
+        user_values = ["TEST: LET A 5", "TEST2: PRINT A", "TEST3: MULT A 5",
+                       "TEST4: INNUM B", "TEST5: DIV A B"]
+        convert_tokens = convert_to_grin_tokens(user_values)
+        all_values = dict()
+        for line in convert_tokens:
+            if line[0].kind() == GrinTokenKind.IDENTIFIER and line[1].kind() == GrinTokenKind.COLON:
+                get_label_command = line[2:]
+                encountered_label_line(get_label_command, all_values)
+        self.assertEqual(all_values, {"A": 5, "B": 5})
